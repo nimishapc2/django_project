ffe6b0a8c4c2ffb078997b36767201529a86cbc1
Nimisha <nimishapc2026@gmail.com>
2026-02-27 06:23:06 +0530

    Updated

--- Shopping_Cart/Shopping_Cart/settings.py
+++ Shopping_Cart/Shopping_Cart/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-u_!(e$$7^xrsm)2ys$qb%vqo(l&qgren+%r8=q7q_(lp-qa_2-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['nimisha.pythonanywhere.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -116,14 +116,10 @@ import os
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_URL = '/static/'
-
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
